5193bf3a065c0984da6ceabb56175d5e6fb0954d
viktorkulygin <mrastonone@gmail.com>
2026-01-11 16:50:20 +0200

    added structure for task 6.1, fixed main structure a bit

---
+++ chapter6/task61/src/main/java/com/kulygin/Main61.java
@@ -0,0 +1,13 @@
+package com.kulygin;
+
+public class Main61 {
+
+    public static void main(String[] args) {
+        // Add code to test the new method
+    }
+
+    public static int mult(int x, int y) {
+        // Implement the recursive multiplication method
+        return 0;
+    }
+}
