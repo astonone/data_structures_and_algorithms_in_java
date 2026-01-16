612517a8de1089a8fba955b61e7b673d128161b9
viktorkulygin <mrastonone@gmail.com>
2026-01-16 15:58:45 +0200

    implemented task 6.2

--- chapter6/task62/src/main/java/com/kulygin/Main62.java
+++ chapter6/task62/src/main/java/com/kulygin/Main62.java
@@ -1,16 +1,43 @@
 package com.kulygin;
 
 public class Main62 {
+    static int width;
+    static int height;
+    static char[][] buffer;
 
     public static void main(String[] args) {
-        // Implement the main method to display the tree
+        width = 16;
+        height = log2(width) + 1;
+        buffer = new char[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                buffer[i][j] = '-';
+            }
+        }
+        makeBranches(0, width - 1, 0);
+        display();
     }
 
-    public static void makeBranches(int left, int right) {
-        // Implement the recursive method to create branches
+    public static void makeBranches(int left, int right, int row) {
+        if (row == buffer.length) {
+            return;
+        }
+        int center = (left + right) / 2;
+        buffer[row][center] = 'X';
+        makeBranches(left, center, row + 1);
+        makeBranches(center + 1, right, row + 1);
     }
 
     public static void display() {
-        // Implement the method to display the tree
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(buffer[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int log2(int N) {
+        return (int)(Math.log(N) / Math.log(2));
     }
 }
